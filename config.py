1c8a1fd192a55b0926bea949493d0069b1197df0
smsm85200 <147330112+smsm85200@users.noreply.github.com>
2023-12-02 08:00:44 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 class Config:
     api_id = 26022994 
     api_hash = "2e84a6b68bd6b5a79f46e8192668e0ea" 
-    TOKEN = "6100362120:AAEASUEw65Kw4JvB4sHdL-eDMj09dqLi3WQ" 
+    tok = "6100362120:AAEASUEw65Kw4JvB4sHdL-eDMj09dqLi3WQ" 
+    idown = "411414467" 
